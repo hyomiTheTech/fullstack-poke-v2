2efd365d66d926205a461f03558738144f526d6a
Eui Hyun Kim <38476995+hyomiTheTech@users.noreply.github.com>
2019-11-27 15:10:21 -0800

    Update config.js

--- database-mysql/config.js
+++ database-mysql/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   user: 'root',
   // if you are using the pairing stations, the user is `student`
-  password: 'Rladmlgus90',
+  password: '',
   // if you are using the paring stations, the password is `student`
   // if you do not have a password, delete this key/value pair from
   // the object
